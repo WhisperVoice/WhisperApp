e15fa30e584d6b4e593ab07eb4acb92e8adcc900
Sabina Cartacio <scart039@fiu.edu>
2019-02-10 18:48:07 -0500

    Compass and location button displays

--- components/home/Home.js
+++ components/home/Home.js
@@ -9,9 +9,9 @@ import {
   Text
 } from 'react-native';
 import {Actions, ActionConst} from 'react-native-router-flux';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
-import arrowImg from '../../assets/images/left-arrow.png';
-import HomeStyles from './HomeStyles'
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import HomeStyles from './HomeStyles';
+import {Constants} from 'expo';
 
 const SIZE = 40;
 
@@ -21,14 +21,21 @@ export default class Home extends Component {
 
     this.state = {
       isLoading: false,
+      flex: 0
     };
 
     this._onPress = this._onPress.bind(this);
     this.growAnimated = new Animated.Value(0);
   }
 
+  componentWillMount() {
+    setTimeout(()=> {
+      this.setState({flex: HomeStyles.map})
+    }, 500);
+  }
+
   _onPress() {
-    if (this.state.isLoading) return;
+    if (this.state.isLoading) retsurn;
 
     this.setState({isLoading: true});
 
@@ -44,24 +51,25 @@ export default class Home extends Component {
   }
 
   render() {
+
     const changeScale = this.growAnimated.interpolate({
       inputRange: [0, 1],
       outputRange: [1, SIZE],
     });
 
     return (
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={ HomeStyles.map }
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          showsUserLocation={true}
-          showsMyLocationButton={true}
-        />
+          <MapView
+            provider={PROVIDER_GOOGLE}
+            style={this.state.flex}
+            initialRegion={{ 
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+          />
     );
   }
 }
